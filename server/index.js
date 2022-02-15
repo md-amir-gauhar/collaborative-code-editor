2822239f072392a9fbe1b14dea05bf72c10434d5
md-amir-gauhar <gauharamir15@gmail.com>
2022-02-15 16:22:45 +0530

    feat: implemented POST /run and starteed writing files using fs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { generateFile } = require('./generateFile')
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
@@ -7,16 +7,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.json({ hello: "bitches" })
+  return res.json({ hello: "bitches" })
 })
 
-app.post("/run", (req, res) => {
+app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body
   if (code === undefined) {
     return res.status(400).json({ success: false, error: "empty code body" })
   }
 
-  return res.json({ language, code })
+  // need to genrerate cpp file with content from the request
+  const filepath = await generateFile(language, code)
+
+  //run the file and send the response
+  return res.json({ filepath })
 })
 
 app.listen(5000, () => {
